9c4d9af9cf00197f715ad54aeb5958cf1a45a2c8
berk2s <berktopcu@outlook.com>
2022-07-02 20:24:06 +0300

    initial commit

---
+++ src/test/java/com/berk2s/omsapi/OmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berk2s.omsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
