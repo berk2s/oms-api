5b919afde2ccf7c60fd05938748d77ea9305dea0
berk2s <berktopcu@outlook.com>
2022-07-06 18:10:03 +0300

    swagger integrated to create order endpoint

--- src/main/java/com/berk2s/omsapi/infra/adapters/order/controller/OrderController.java
+++ src/main/java/com/berk2s/omsapi/infra/adapters/order/controller/OrderController.java
@@ -5,6 +5,14 @@ import com.berk2s.omsapi.domain.order.usecase.CreateOrder;
 import com.berk2s.omsapi.domain.usecase.UseCaseHandler;
 import com.berk2s.omsapi.infra.adapters.order.controller.dtos.CreateOrderRequest;
 import com.berk2s.omsapi.infra.adapters.order.controller.dtos.OrderResponse;
+import com.berk2s.omsapi.infra.exception.handler.dto.ErrorResponse;
+import com.berk2s.omsapi.infra.swagger.SwaggerExample;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,6 +31,31 @@ public class OrderController {
 
     private final UseCaseHandler<Order, CreateOrder> createOrderUseCaseHandler;
 
+    @Operation(summary = "Create Order")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Order created", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = OrderResponse.class),
+                            examples = {
+                                    @ExampleObject(name = "Successfully created Order", value = SwaggerExample.ORDER_CREATED_RESPONSE)
+                            })
+            }),
+            @ApiResponse(responseCode = "400", description = "Invalid request", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(name = "Bad request", value = SwaggerExample.CREATE_ORDER_REQ_INVALID),
+                                    @ExampleObject(name = "Out of bounds quantity",
+                                            description = "Occurs when requested quantity is higher than inventory quantity",
+                                            value = SwaggerExample.CREATE_ORDER_OUT_OF_BOUNDS_ERROR),
+                            })
+            }),
+            @ApiResponse(responseCode = "404", description = "Not founds", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(name = "Customer not found", value = SwaggerExample.CUSTOMER_NOT_FOUND),
+                                    @ExampleObject(name = "Product not found", value = SwaggerExample.PRODUCT_NOT_FOUND),
+                            })
+            })
+    })
     @PostMapping(value = "/{customerId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<OrderResponse> createOrder(@PathVariable UUID customerId,
                                                      @Valid @RequestBody CreateOrderRequest req) {
