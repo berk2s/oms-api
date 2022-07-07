d532343bbcce8a7020a739425459fe1e023a0c01
berk2s <berktopcu@outlook.com>
2022-07-07 14:06:53 +0300

    enhanced build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 echo "Project is being running"
 docker-compose rm -f
-docker rmi berk2s/oms-api
-docker rmi berk2s/oms-api -f
-docker build -t berk2s/oms-api .
+docker rmi berk2s/oms-api:main
+docker rmi berk2s/oms-api:main -f
+docker build -t berk2s/oms-api:main . -f Dockerfile.ci
 docker compose up
 read -p "Press any key for terminate process"
 docker compose down
 docker compose rm -f
-docker rmi berk2s/oms-api
-docker rmi berk2s/oms-api -f
+docker rmi berk2s/oms-api:main
+docker rmi berk2s/oms-api:main -f
 echo "Images and containers deleted"
